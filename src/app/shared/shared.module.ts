ca57bebbc7eb01c895cdcd03f03282f80013fd89
soRodriguezz <s.rodriguezzapata01@gmail.com>
2021-07-17 17:06:16 -0400

    sidemenu - lazyload

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,18 @@
+import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SidemenuComponent } from './sidemenu/sidemenu.component';
+
+
+
+@NgModule({
+  declarations: [SidemenuComponent],
+  imports: [
+    CommonModule,
+    RouterModule
+  ],
+  exports: [
+    SidemenuComponent
+  ]
+})
+export class SharedModule { }
