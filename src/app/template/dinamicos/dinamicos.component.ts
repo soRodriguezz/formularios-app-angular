1b83cf5d93b86ca3c80d23359cded13c4006d5b3
sorodriguezz <sebastian.rodriguez19@outlook.com>
2021-09-17 16:02:20 -0300

    Fin seccion template forms

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,4 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+interface Persona {
+  nombre: string;
+  favoritos: Favorito[];
+}
+
+interface Favorito {
+  id: number;
+  nombre: string;
+}
 
 @Component({
   selector: 'app-dinamicos',
@@ -6,15 +16,31 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class DinamicosComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class DinamicosComponent {
+  nuevoJuego: string = '';
+  persona: Persona = {
+    nombre: 'Sebastian',
+    favoritos: [
+      { id: 1, nombre: 'Metal Gear'},
+      { id: 2, nombre: 'DeathStranding'}
+    ]
   }
 
   guardar() {
     console.log('Formulario posteado');
+  }
+
+  agregarJuego() {
+    const nuevoFavorito: Favorito = {
+      id: this.persona.favoritos.length + 1,
+      nombre: this.nuevoJuego
+    }
+    this.persona.favoritos.push( { ...nuevoFavorito } );
+    this.nuevoJuego = '';
+  }
+
+  eliminar( index: number ) {
+    this.persona.favoritos.splice(index, 1)
   }
 
 }
